37190558e815c1a4941ead3083265453281feaad
배지훈 <new0126@naver.com>
2026-01-06 22:34:03 +0900

    feat: modal 컴포넌트 구현 및 예제 코드 구현
    - modal 컴포넌트 및 예저 코드 구현
    - type aliasing 을 위한 tsconfig 설정 수정
    - readme.md 파일에 폴더 구조 작성
    - Client Only Wrapper 컴포넌트 구현

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode, useState } from "react";
+import { ReactNode, Suspense, useState } from "react";
 import { queryClient } from "../lib/queryClient";
+import { ClientOnly } from "@/components/common/ClientOnly";
+import { ModalRenderer } from "@/components/common/Modal";
 
 export default function Providers({ children }: { children: ReactNode }) {
   const [client] = useState(() => queryClient);
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ClientOnly>
+        <Suspense fallback={null}>
+          <ModalRenderer />
+        </Suspense>
+      </ClientOnly>
+    </QueryClientProvider>
+  );
 }
